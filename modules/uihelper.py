e6e6b64365ac4a7f54f12234536083aaf250dd61
berryrock <berry.rck@gmail.com>
2020-02-20 16:46:09 +0300

    v0.7.3

--- modules/uihelper.py
+++ modules/uihelper.py
@@ -92,7 +92,7 @@ def alleged(chat, item):
 	date_time = item.get("date_time", None)
 	if date_time:
 		date_time = datetime.datetime.strptime(date_time, '%Y-%m-%dT%H:%M:%f%z')
-		date_time = date_time.strptime('%d %m %Y, %H:%M:%S')
+		date_time = date_time.strftime('%d %m %Y, %H:%M:%S')
 	else:
 		date_time = 'unknown'
 	message = translation["rus"].text_alleged.format(item["dish"]["name"],date_time)
